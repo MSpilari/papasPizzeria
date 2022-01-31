7fcbb20ea517894cd1760fe63cb8f6fe201017b7
MSpilari <mb.spilari@hotmail.com>
2022-01-31 17:44:55 -0300

    Inserindo as cores de acordo com o styleguide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@ module.exports = {
 	mode: 'jit',
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}'
+		'./src/components/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				guideRed: '#ee1822',
+				guideOrange: '#fa7921',
+				guideOrangeLight: '#fe9920',
+				guideGreen: '#029e5e',
+				guideYellow: '#fbbf01'
+			},
+			backgroundImage: {}
+		}
 	},
 	plugins: []
 }
