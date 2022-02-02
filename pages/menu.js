622df6dbfc23d06eff872ddfc370a1fdb548c420
MSpilari <mb.spilari@hotmail.com>
2022-02-02 12:57:01 -0300

    Criando a página de menu

---
+++ pages/menu.js
@@ -0,0 +1,13 @@
+import { Header } from '../src/components/Header'
+
+const Menu = () => {
+	return (
+		<main className='w-screen h-screen'>
+			<div className='w-full h-full'>
+				<Header />
+			</div>
+		</main>
+	)
+}
+
+export default Menu
