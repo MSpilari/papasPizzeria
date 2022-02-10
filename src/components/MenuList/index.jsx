93f7f1f95155370c1741cc72e7fd7f6ed317a844
MSpilari <mb.spilari@hotmail.com>
2022-02-10 17:36:31 -0300

    Recebendo os dados da API e passando eles para os PizzaCards

--- src/components/MenuList/index.jsx
+++ src/components/MenuList/index.jsx
@@ -1,18 +1,24 @@
 import { PizzaCard } from '../PizzaCard'
 
-const MenuList = () => {
+const MenuList = ({ allPizzas }) => {
 	return (
 		<div className='w-full mt-2 flex flex-col items-center justify-center'>
 			<h1 className='text-lg font-bold text-guideOrange'>
 				What do you like to order ?
 			</h1>
-			<PizzaCard />
-			<PizzaCard />
-			<PizzaCard />
-			<PizzaCard />
-			<PizzaCard />
-			<PizzaCard />
-			<PizzaCard />
+			{allPizzas.map(pizza => {
+				const { name, image, description, price, id } = pizza
+				return (
+					<PizzaCard
+						key={id}
+						id={id}
+						name={name}
+						image={image}
+						description={description}
+						price={price}
+					/>
+				)
+			})}
 		</div>
 	)
 }
