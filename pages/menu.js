4d87bf93e571f9c3472f6c6ef854971946c86c18
MSpilari <mb.spilari@hotmail.com>
2022-02-10 17:35:06 -0300

    Fazendo a chamada para a API, via getServerSideProps e repassando o conteúdo para o MenuList

--- pages/menu.js
+++ pages/menu.js
@@ -2,18 +2,28 @@ import { Featured } from '../src/components/Featured'
 import { Header } from '../src/components/Header'
 import { MenuList } from '../src/components/MenuList'
 
-const Menu = () => {
+const Menu = ({ data }) => {
 	return (
 		<main className='w-screen h-screen'>
 			<div className='w-full h-full'>
 				<Header />
 				<div className='w-full h-[calc(100%-160px)] overflow-y-auto'>
 					<Featured />
-					<MenuList />
+					<MenuList allPizzas={data} />
 				</div>
 			</div>
 		</main>
 	)
+}
+
+export async function getServerSideProps() {
+	const res = await fetch('http://localhost:3000/api/allPizzas')
+
+	const data = await res.json()
+
+	return {
+		props: { data }
+	}
 }
 
 export default Menu
