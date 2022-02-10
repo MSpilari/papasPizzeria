4cf3e7b43505d06e96ff130fbe0c780551413814
MSpilari <mb.spilari@hotmail.com>
2022-02-10 17:38:53 -0300

    Inserindo as infos da API no Card e criando o Link para a página que mostra as infos detalhadas sobre a pizza

--- src/components/PizzaCard/index.jsx
+++ src/components/PizzaCard/index.jsx
@@ -1,36 +1,39 @@
 import Image from 'next/image'
 import { AiOutlineHeart } from 'react-icons/ai'
-import Pizza1 from '../../assets/pizza1.jpg'
+import Link from 'next/link'
 
-const PizzaCard = () => {
+const PizzaCard = ({ name, description, image, price, id }) => {
 	return (
-		<div className='w-[90%] max-w-xs h-[200px] flex flex-col items-center border-2 border-slate-300 rounded-3xl overflow-hidden my-2'>
-			<div className='relative w-full h-full'>
-				<Image
-					src={Pizza1}
-					objectFit='cover'
-					layout='fill'
-					sizes='50vw'
-					alt='Pizza Image'
-				/>
+		<Link href={`/pizza/${id}`} passHref>
+			<div className='w-[90%] max-w-xs h-[200px] flex flex-col items-center border-2 border-slate-300 rounded-3xl overflow-hidden my-2'>
+				<div className='relative w-full h-full'>
+					<Image
+						src={image}
+						objectFit='cover'
+						layout='fill'
+						sizes='50vw'
+						alt='Pizza Image'
+					/>
+				</div>
+				<div className=' w-[85%] mx-auto my-1 flex items-center justify-between'>
+					<span className='flex items-center border-2 border-slate-300 rounded-3xl px-1 py-1 font-bold'>
+						{' '}
+						<span className='text-guideOrange text-lg mr-1'>$</span>
+						{price.toLocaleString('en-US', {
+							style: 'currency',
+							currency: 'USD'
+						})}
+					</span>
+					<button className='text-2xl'>
+						<AiOutlineHeart />
+					</button>
+				</div>
+				<div className='w-[90%] flex flex-col flex-nowrap mx-auto'>
+					<p className='text-sm mb-1 truncate'>{name}</p>
+					<p className='text-xs truncate text-gray-400'>{description}</p>
+				</div>
 			</div>
-			<div className=' w-[85%] mx-auto my-1 flex items-center justify-between'>
-				<span className='flex items-center border-2 border-slate-300 rounded-3xl px-1 py-1 font-bold'>
-					{' '}
-					<span className='text-guideOrange text-lg mr-1'>$</span>
-					10,00
-				</span>
-				<button className='text-2xl'>
-					<AiOutlineHeart />
-				</button>
-			</div>
-			<div className='w-[90%] flex flex-col flex-nowrap mx-auto'>
-				<p className='text-sm mb-1 truncate'>Chicken Hawaiian</p>
-				<p className='text-xs truncate text-gray-400'>
-					Chicken, Cheese and Pineapple
-				</p>
-			</div>
-		</div>
+		</Link>
 	)
 }
 
