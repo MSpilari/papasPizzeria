e77771258a39c34f17731f6b7b15483f7e211728
MSpilari <mb.spilari@hotmail.com>
2022-02-14 17:21:59 -0300

    Escrevendo o esqueleto da página com dados fictícios

--- src/components/PizzaDetails/index.jsx
+++ src/components/PizzaDetails/index.jsx
@@ -1,5 +1,62 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 const PizzaDetails = () => {
-	return <h1>Pizza Details</h1>
+	return (
+		<div className='w-full h-full flex flex-col items-center'>
+			<div className='flex flex-col w-[90%] h-[200px] mx-auto'>
+				<div>
+					<button>
+						<Link href='/menu'>Back</Link>
+					</button>
+					<button>Love</button>
+				</div>
+				<div className='relative w-full h-full'>
+					<Image
+						objectFit='cover'
+						layout='fill'
+						sizes='50vw'
+						src='https://github.com/MSpilari.png'
+						alt='Pizza Image'
+					/>
+				</div>
+			</div>
+			<h1>Title</h1>
+			<div>
+				<span>Price</span>
+				<div>
+					<button>Minus</button>
+					<p>Quantity</p>
+					<button>Plus</button>
+				</div>
+			</div>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
+				explicabo voluptates reiciendis magni, accusantium dolore exercitationem
+				molestias inventore, incidunt libero vel. Repudiandae est dolorum
+				sapiente a velit, deleniti voluptatibus laboriosam!
+			</p>
+			<div>
+				<h2>Choose an Add On</h2>
+				<div>
+					<label htmlFor='More Cheese'>Extra Cheese</label>
+					<span>Price</span>
+					<input type='checkbox' name='More Cheese' id='More Cheese' />
+				</div>
+				<div>
+					<label htmlFor='More Cheese'>Extra Cheese</label>
+					<span>Price</span>
+					<input type='checkbox' name='More Cheese' id='More Cheese' />
+				</div>
+				<div>
+					<label htmlFor='More Cheese'>Extra Cheese</label>
+					<span>Price</span>
+					<input type='checkbox' name='More Cheese' id='More Cheese' />
+				</div>
+			</div>
+			<button>Add to Cart</button>
+		</div>
+	)
 }
 
 export { PizzaDetails }
