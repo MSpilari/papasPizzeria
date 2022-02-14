c5da092fce7bd786e874137c1f321bbd3c6e07ee
MSpilari <mb.spilari@hotmail.com>
2022-02-14 13:09:58 -0300

    Colocando o header e fazendo a chamada para a API

--- pages/pizza/[pizzaID].js
+++ pages/pizza/[pizzaID].js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react'
+import { Header } from '../../src/components/Header'
+import { PizzaDetails } from '../../src/components/PizzaDetails'
+
 const PizzaInfo = () => {
-	return <h1>Ok !</h1>
+	const [pizzaData, setPizzaData] = useState(null)
+
+	useEffect(() => {
+		const [_, pizzaID] = document.location.pathname.split('/pizza/')
+
+		async function fetchData() {
+			const res = await fetch(`http://localhost:3000/api/pizzas/${pizzaID}`)
+			const data = await res.json()
+			return setPizzaData(data)
+		}
+
+		fetchData()
+	}, [])
+
+	return (
+		<main className='w-screen h-screen'>
+			<div className='w-full h-full'>
+				<Header />
+				<div className='w-full h-[calc(100%-160px)] overflow-y-auto'>
+					{!pizzaData ? <h1>Loading...</h1> : <PizzaDetails />}
+				</div>
+			</div>
+		</main>
+	)
 }
 
 export default PizzaInfo
