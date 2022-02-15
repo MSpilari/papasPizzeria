bd457302ad5d2bcc6ed2924d28e2326cff62ffb6
MSpilari <mb.spilari@hotmail.com>
2022-02-15 13:12:09 -0300

    Inserindo os dados vindos da Api e fazendo a estilização

--- src/components/PizzaDetails/index.jsx
+++ src/components/PizzaDetails/index.jsx
@@ -1,60 +1,101 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import {
+	AiOutlineArrowLeft,
+	AiOutlineHeart,
+	AiFillHeart,
+	AiOutlineMinus,
+	AiOutlinePlus,
+	AiOutlineShoppingCart
+} from 'react-icons/ai'
+import { formatter } from '../../utils/currencyFormatter'
 
-const PizzaDetails = () => {
+const PizzaDetails = ({ pizzaData }) => {
+	const { image, description, likes, name, optional, price } = pizzaData
+
 	return (
-		<div className='w-full h-full flex flex-col items-center'>
-			<div className='flex flex-col w-[90%] h-[200px] mx-auto'>
-				<div>
-					<button>
-						<Link href='/menu'>Back</Link>
+		<div className='w-full h-full flex flex-col'>
+			<div className='flex flex-col w-[90%] mx-auto'>
+				<div
+					className='flex items-center justify-between 
+							w-[90%] mx-auto text-xl z-10 relative top-10'
+				>
+					<button className='bg-white rounded-lg p-1 '>
+						<Link href='/menu' passHref>
+							<AiOutlineArrowLeft />
+						</Link>
 					</button>
-					<button>Love</button>
+					<button className='bg-white rounded-lg p-1 text-red-500 '>
+						<AiOutlineHeart />
+					</button>
 				</div>
-				<div className='relative w-full h-full'>
+
+				<div className='relative w-full h-[200px]'>
 					<Image
 						objectFit='cover'
 						layout='fill'
 						sizes='50vw'
-						src='https://github.com/MSpilari.png'
+						src={image}
 						alt='Pizza Image'
 					/>
 				</div>
 			</div>
-			<h1>Title</h1>
-			<div>
-				<span>Price</span>
-				<div>
-					<button>Minus</button>
+			<h1 className='w-[90%]  h-10 mx-auto text-xl my-1 truncate'>{name}</h1>
+			<div className='w-[90%] mx-auto flex items-center justify-between'>
+				<span className='text-2xl text-guideOrange'>
+					{formatter.format(price)}
+				</span>
+
+				<div className='flex items-center'>
+					<button
+						className='border-2 border-guideOrange rounded-full p-1 mr-1 
+					text-black text-lg'
+					>
+						<AiOutlineMinus />
+					</button>
 					<p>Quantity</p>
-					<button>Plus</button>
+					<button
+						className='border-2 border-guideOrange bg-guideOrange rounded-full p-1 ml-1 
+					text-white text-lg'
+					>
+						<AiOutlinePlus />
+					</button>
 				</div>
 			</div>
-			<p>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
-				explicabo voluptates reiciendis magni, accusantium dolore exercitationem
-				molestias inventore, incidunt libero vel. Repudiandae est dolorum
-				sapiente a velit, deleniti voluptatibus laboriosam!
+			<p className='w-[90%] mx-auto my-2 text-sm text-gray-500'>
+				{description}
 			</p>
-			<div>
-				<h2>Choose an Add On</h2>
-				<div>
-					<label htmlFor='More Cheese'>Extra Cheese</label>
-					<span>Price</span>
-					<input type='checkbox' name='More Cheese' id='More Cheese' />
-				</div>
-				<div>
-					<label htmlFor='More Cheese'>Extra Cheese</label>
-					<span>Price</span>
-					<input type='checkbox' name='More Cheese' id='More Cheese' />
-				</div>
-				<div>
-					<label htmlFor='More Cheese'>Extra Cheese</label>
-					<span>Price</span>
-					<input type='checkbox' name='More Cheese' id='More Cheese' />
+			<div className='w-[90%] mx-auto flex flex-col'>
+				<h2 className='my-1 w-full'>Choice of Add On</h2>
+				<div className='flex flex-col w-full'>
+					{optional.map((option, index) => {
+						const { ingredient, optPrice } = option
+						return (
+							<div key={index} className='w-[95%] mx-auto flex items-center'>
+								<label htmlFor={ingredient}>{ingredient}</label>
+								<span className='ml-auto text-guideOrange'>
+									+ {formatter.format(optPrice)}
+								</span>
+								<input
+									className='ml-1'
+									type='checkbox'
+									name={ingredient}
+									id={ingredient}
+								/>
+							</div>
+						)
+					})}
 				</div>
 			</div>
-			<button>Add to Cart</button>
+			<button
+				className='flex items-center mx-auto my-2 py-1 
+								px-2 bg-guideOrange rounded-full text-white'
+			>
+				<i className='text-2xl text-black bg-white rounded-full p-1 mr-1'>
+					<AiOutlineShoppingCart />
+				</i>
+				Add to Cart
+			</button>
 		</div>
 	)
 }
