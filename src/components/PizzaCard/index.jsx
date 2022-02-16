d59e2b7dd827d6c30f726de1bfe16005587a9e32
MSpilari <mb.spilari@hotmail.com>
2022-02-16 12:51:49 -0300

    Inserindo a formatação do preço

--- src/components/PizzaCard/index.jsx
+++ src/components/PizzaCard/index.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { AiOutlineHeart } from 'react-icons/ai'
 import Link from 'next/link'
+import { formatter } from '../../utils/currencyFormatter'
 
 const PizzaCard = ({ name, description, image, price, id }) => {
 	return (
@@ -18,11 +19,9 @@ const PizzaCard = ({ name, description, image, price, id }) => {
 				<div className=' w-[85%] mx-auto my-1 flex items-center justify-between'>
 					<span className='flex items-center border-2 border-slate-300 rounded-3xl px-1 py-1 font-bold'>
 						{' '}
-						<span className='text-guideOrange text-lg mr-1'>$</span>
-						{price.toLocaleString('en-US', {
-							style: 'currency',
-							currency: 'USD'
-						})}
+						<span className='text-guideOrange text-lg mr-1'>
+							{formatter.format(price)}
+						</span>
 					</span>
 					<button className='text-2xl'>
 						<AiOutlineHeart />
