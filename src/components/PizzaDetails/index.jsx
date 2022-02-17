1c47d15fe619199ffb9d4006b971635ffdc60639
MSpilari <mb.spilari@hotmail.com>
2022-02-17 17:45:32 -0300

    Criando o fullCost, fazendo o envio do pedido para o carrinho por meio do dispatch do Redux

--- src/components/PizzaDetails/index.jsx
+++ src/components/PizzaDetails/index.jsx
@@ -1,27 +1,34 @@
 import { useState } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { useDispatch } from 'react-redux'
+import { cartAdded } from '../../redux/slices/CartSlice'
 import { BackLikeImage } from './BackLikeImage'
 import { Optionals } from './Optionals'
 import { TitlePriceQtyDesc } from './TitlePriceQtyDesc'
 
 const PizzaDetails = ({ pizzaData }) => {
 	const { image, description, likes, name, optional, price } = pizzaData
-
+	const [quantity, setQuantity] = useState(1)
 	const [addOns, setAddOns] = useState([])
 
 	const addOnsCost = addOns
 		.map(element => Number(element.price))
 		.reduce((prevValue, currentValue) => (prevValue += currentValue), 0)
 
+	const fullCost = price * quantity + addOnsCost
+
+	const dispatch = useDispatch()
+
 	return (
 		<div className='w-full h-full flex flex-col'>
 			<BackLikeImage image={image} />
 
 			<TitlePriceQtyDesc
-				addOnsCost={addOnsCost}
 				description={description}
 				name={name}
-				price={price}
+				quantity={quantity}
+				setQuantity={setQuantity}
+				fullCost={fullCost}
 			/>
 
 			<Optionals optional={optional} addOns={addOns} setAddOns={setAddOns} />
@@ -29,6 +36,17 @@ const PizzaDetails = ({ pizzaData }) => {
 			<button
 				className='flex items-center mx-auto my-2 py-1 
 								px-2 bg-guideOrange rounded-full text-white'
+				onClick={() => {
+					dispatch(
+						cartAdded({
+							image,
+							name,
+							fullCost,
+							quantity,
+							addOns
+						})
+					)
+				}}
 			>
 				<i className='text-2xl text-black bg-white rounded-full p-1 mr-1'>
 					<AiOutlineShoppingCart />
