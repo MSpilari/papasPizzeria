9fa38f3fe03ae1ba2edc322c4f9517318ef84fec
MSpilari <mb.spilari@hotmail.com>
2022-02-19 12:54:09 -0300

    Separando os reducers de dentro do slice

--- src/redux/slices/CartSlice.js
+++ src/redux/slices/CartSlice.js
@@ -1,4 +1,6 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import { CartAddedReducer } from '../reducers/cart/CartAddedReducer'
+import { CartRemovedReducer } from '../reducers/cart/CartRemovedReducer'
 
 const initialValue = []
 
@@ -6,24 +8,8 @@ const CartSlice = createSlice({
 	name: 'cart',
 	initialState: initialValue,
 	reducers: {
-		cartAdded: {
-			reducer(state, action) {
-				state.push(action.payload)
-			},
-			prepare(chosenPizza) {
-				const id = nanoid()
-				return {
-					payload: {
-						id,
-						...chosenPizza
-					}
-				}
-			}
-		},
-		cartRemoved(state, action) {
-			const { id } = action.payload
-			return state.filter(pizza => pizza.id != id)
-		}
+		cartAdded: CartAddedReducer,
+		cartRemoved: CartRemovedReducer
 	}
 })
 
