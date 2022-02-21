359977d462c4610c65d4803472deaf0c2a642583
MSpilari <mb.spilari@hotmail.com>
2022-02-21 16:28:45 -0300

    Inserindo a altura do componente para telas large

--- pages/menu.js
+++ pages/menu.js
@@ -7,7 +7,7 @@ const Menu = ({ data }) => {
 		<main className='w-screen h-screen'>
 			<div className='w-full h-full'>
 				<Header />
-				<div className='w-full h-[calc(100%-160px)] overflow-y-auto'>
+				<div className='w-full h-[calc(100%-160px)] lg:h-[calc(100%-96px)] overflow-y-auto'>
 					<Featured />
 					<MenuList allPizzas={data} />
 				</div>
