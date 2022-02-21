c34124db1cfa811aa25233a20f744c1ddfeac690
MSpilari <mb.spilari@hotmail.com>
2022-02-21 17:13:00 -0300

    Especificando onde começa a linha/coluna do componentee alterando o tamanho da imagem, apenas para telas maiores

--- src/components/PizzaDetails/BackLikeImage.jsx
+++ src/components/PizzaDetails/BackLikeImage.jsx
@@ -4,7 +4,7 @@ import { AiOutlineArrowLeft, AiOutlineHeart } from 'react-icons/ai'
 
 const BackLikeImage = ({ image }) => {
 	return (
-		<div className='flex flex-col w-[90%] mx-auto'>
+		<div className='flex flex-col w-[90%] mx-auto lg:col-span-1 lg:row-span-2'>
 			<div
 				className='flex items-center justify-between 
 							w-[90%] mx-auto text-xl z-10 relative top-10'
@@ -20,7 +20,7 @@ const BackLikeImage = ({ image }) => {
 				</button>
 			</div>
 
-			<div className='relative w-full h-[200px] rounded-lg overflow-hidden'>
+			<div className='relative w-full h-[200px] lg:h-full rounded-lg overflow-hidden'>
 				<Image
 					objectFit='cover'
 					priority
