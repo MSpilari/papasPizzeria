87193cbc9d00f4bdd35b8377bcf8a703dc963561
MSpilari <mb.spilari@hotmail.com>
2022-02-22 12:59:42 -0300

    Fazendo a home retornar apenas o introduction, pois o Welcome se torna uma página de login

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import { Introduction } from '../src/components/Introduction'
-import { Welcome } from '../src/components/Welcome'
 
 export default function Home() {
-	const [toLogin, setToLogin] = useState(false)
 	return (
 		<main className='w-screen h-screen'>
 			<div className='w-full h-full'>
-				{!toLogin ? <Introduction setToLogin={setToLogin} /> : <Welcome />}
+				<Introduction />
 			</div>
 		</main>
 	)
