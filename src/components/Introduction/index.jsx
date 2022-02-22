0fe92282da8399cff08caa1b06173b13940930e3
MSpilari <mb.spilari@hotmail.com>
2022-02-22 13:00:46 -0300

    Transformando o Intro num Link que envia o usuário para a página de login

--- src/components/Introduction/index.jsx
+++ src/components/Introduction/index.jsx
@@ -1,21 +1,23 @@
 import Image from 'next/image'
 import BigLogo from '../../assets/BigLogo.png'
+import Link from 'next/link'
 
-const Introduction = ({ setToLogin }) => {
+const Introduction = () => {
 	return (
-		<div
-			className='w-full h-full flex flex-col items-center 
+		<Link href='/login' passHref>
+			<div
+				className='w-full h-full flex flex-col items-center 
 				justify-center bg-orange-700'
-		>
-			<Image
-				onClick={() => setToLogin(true)}
-				objectFit='contain'
-				className='animate-pulse'
-				alt='Papa`s Pizzeria Logo'
-				src={BigLogo}
-			/>
-			<p className='mt-1 text-xl text-guideOrangeLight'>Click to Open...</p>
-		</div>
+			>
+				<Image
+					objectFit='contain'
+					className='animate-pulse'
+					alt='Papa`s Pizzeria Logo'
+					src={BigLogo}
+				/>
+				<p className='mt-1 text-xl text-guideOrangeLight'>Click to Open...</p>
+			</div>
+		</Link>
 	)
 }
 
