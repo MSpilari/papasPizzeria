87a4b06658465763d3937838fb1a44c446bf6fc7
MSpilari <mb.spilari@hotmail.com>
2022-02-26 14:19:02 -0300

    Fazendo a chamada para a API que retorna o id do firebaseDoc e com esse valor incrementando a session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,11 +15,16 @@ export default NextAuth({
 	},
 	callbacks: {
 		async session({ session, token, user }) {
-			session.user.username = session.user.name
-				.split(' ')
-				.join('')
-				.toLowerCase()
-			session.accessToken = token.sub
+			const res = await fetch('http://localhost:3000/api/users/findUserId', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ email: session.user.email })
+			})
+
+			const data = await res.json()
+			session.user.firebaseID = data.userID
 			return session
 		},
 		async signIn({ account, email, profile, user, credentials }) {
