32640918a218d95b566066baef174d6aef5165e9
MSpilari <mb.spilari@hotmail.com>
2022-03-01 12:20:25 -0300

    Escrevendo a lógica dos likes adicionando ou removendo do db a partir do clique do usuário

--- src/components/PizzaCard/index.jsx
+++ src/components/PizzaCard/index.jsx
@@ -1,15 +1,21 @@
-import { doc, setDoc } from 'firebase/firestore'
+import {
+	collection,
+	deleteDoc,
+	doc,
+	onSnapshot,
+	setDoc
+} from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
-import { AiOutlineHeart } from 'react-icons/ai'
+import { useEffect, useState } from 'react'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { db } from '../../../firebase'
 import { formatter } from '../../utils/currencyFormatter'
 
 const PizzaCard = ({ name, description, image, price, id }) => {
 	const { data: session } = useSession()
-
+	const [likes, setLikes] = useState([])
 	const [hasLiked, setHasLiked] = useState(false)
 
 	const likePizza = async () => {
@@ -18,10 +24,25 @@ const PizzaCard = ({ name, description, image, price, id }) => {
 		 - the usual way "NODE_ENV" 
 		 - for Next, following the "NEXT_PUBLIC_NODE_ENV" pattern. 
 		So you can use the FRONT or BACK approach.  */
-		await setDoc(doc(db, 'users', session.user.firebaseID, 'likes', id), {
-			name
-		})
+		hasLiked
+			? await deleteDoc(doc(db, 'users', session.user.firebaseID, 'likes', id))
+			: await setDoc(doc(db, 'users', session.user.firebaseID, 'likes', id), {
+					name,
+					pizzaId: id
+			  })
 	}
+
+	useEffect(() => {
+		session &&
+			onSnapshot(
+				collection(db, 'users', session.user.firebaseID, 'likes'),
+				snapshot => setLikes(snapshot.docs.map(like => like.data()))
+			)
+	}, [session])
+
+	useEffect(() => {
+		setHasLiked(likes.findIndex(like => like.pizzaId === id) != -1)
+	}, [id, likes])
 
 	return (
 		<div className='w-[90%] max-w-xs h-[200px] flex flex-col items-center border-2 border-slate-300 rounded-3xl overflow-hidden my-2 lg:mx-auto'>
@@ -44,7 +65,11 @@ const PizzaCard = ({ name, description, image, price, id }) => {
 					</span>
 				</span>
 				<button onClick={() => likePizza()} className='text-2xl'>
-					<AiOutlineHeart />
+					{hasLiked ? (
+						<AiFillHeart className='text-red-500' />
+					) : (
+						<AiOutlineHeart />
+					)}
 				</button>
 			</div>
 			<div className='w-[90%] flex flex-col flex-nowrap mx-auto'>
