577c381e5ac0145e4ca7b83ee35178d203b5a5a7
MSpilari <mb.spilari@hotmail.com>
2022-03-01 17:16:24 -0300

    Fazendo o botão de likes adicionar ou retirar os likes

--- src/components/PizzaDetails/BackLikeImage.jsx
+++ src/components/PizzaDetails/BackLikeImage.jsx
@@ -1,8 +1,44 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { AiOutlineArrowLeft, AiOutlineHeart } from 'react-icons/ai'
+import { AiOutlineArrowLeft, AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
+import { useSession } from 'next-auth/react'
+import { useState, useEffect } from 'react'
+import {
+	deleteDoc,
+	doc,
+	setDoc,
+	onSnapshot,
+	collection
+} from 'firebase/firestore'
+import { db } from '../../../firebase'
 
 const BackLikeImage = ({ image }) => {
+	const { data: session } = useSession()
+	const [hasLiked, setHasLiked] = useState(false)
+	const [likes, setLikes] = useState([])
+	const [_, id] = document.location.pathname.split('/pizza/')
+
+	const likePizza = async () => {
+		return session && hasLiked
+			? await deleteDoc(doc(db, 'users', session.user.firebaseID, 'likes', id))
+			: await setDoc(doc(db, 'users', session.user.firebaseID, 'likes', id), {
+					name,
+					pizzaId: id
+			  })
+	}
+
+	useEffect(() => {
+		session &&
+			onSnapshot(
+				collection(db, 'users', session.user.firebaseID, 'likes'),
+				snapshot => setLikes(snapshot.docs.map(like => like.data()))
+			)
+	}, [session])
+
+	useEffect(() => {
+		setHasLiked(likes.findIndex(like => like.pizzaId === id) != -1)
+	}, [id, likes])
+
 	return (
 		<div className='flex flex-col w-[90%] mx-auto lg:col-span-1 lg:row-span-2'>
 			<div
@@ -15,8 +51,13 @@ const BackLikeImage = ({ image }) => {
 					</button>
 				</Link>
 
-				<button className='bg-white rounded-lg p-1 text-red-500 '>
-					<AiOutlineHeart />
+				<button
+					disabled={session === null}
+					onClick={() => likePizza()}
+					className='bg-white rounded-lg p-1 text-red-500 
+									   disabled:text-gray-400 disabled:border-gray-400 disabled:cursor-not-allowed'
+				>
+					{hasLiked ? <AiFillHeart /> : <AiOutlineHeart />}
 				</button>
 			</div>
 
