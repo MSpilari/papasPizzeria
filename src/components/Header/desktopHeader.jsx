2c24b479c01a13773a8e2c7f7f45bb0730d4e4ac
MSpilari <mb.spilari@hotmail.com>
2022-03-02 12:35:38 -0300

    Inserindo o NavigationButtons

--- src/components/Header/desktopHeader.jsx
+++ src/components/Header/desktopHeader.jsx
@@ -1,35 +1,13 @@
-import { collection, onSnapshot } from 'firebase/firestore'
-import { useSession } from 'next-auth/react'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { AiFillCompass } from 'react-icons/ai'
-import { BsFillBellFill, BsFillHeartFill } from 'react-icons/bs'
-import { FaShoppingBag } from 'react-icons/fa'
-import { MdPlace } from 'react-icons/md'
-import { useSelector } from 'react-redux'
-import { db } from '../../../firebase'
+import { useState } from 'react'
 import { SideProfile } from '../SideProfile'
 import { LogoPapasPizzeria } from '../UI/LogoPapasPizzeria'
+import { NavigationButtons } from './CommonParts/NavigationButtons'
 import { SideMenuButton } from '../UI/SideMenuButton'
 import { UserAvatar } from '../UI/UserAvatar'
 
 const DesktopHeader = () => {
 	const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
-	const { data: session, status } = useSession()
-	const [likesCounter, setLikesCounter] = useState(0)
-	const cartLength = useSelector(state => state.cart.length)
 
-	useEffect(() => {
-		let unmounted = false
-		session &&
-			!unmounted &&
-			onSnapshot(
-				collection(db, 'users', session.user.firebaseID, 'likes'),
-				snapshot => setLikesCounter(snapshot.docs.length)
-			)
-		// Prevent a memory leak
-		return () => (unmounted = true)
-	}, [session])
 	return (
 		<>
 			{isSideMenuOpen && <SideProfile />}
@@ -45,55 +23,7 @@ const DesktopHeader = () => {
 				<LogoPapasPizzeria />
 
 				<nav className='flex items-center w-56 ml-auto'>
-					<button className='flex items-center justify-center text-2xl h-full w-full text-slate-500'>
-						<i>
-							<AiFillCompass />
-						</i>
-					</button>
-					<button className='flex items-center justify-center text-2xl h-full w-full text-slate-500'>
-						<i>
-							<MdPlace />
-						</i>
-					</button>
-					<Link href='/cart' passHref>
-						<button
-							className={`relative flex flex-col items-center justify-center text-2xl h-full w-full 
-												 ${cartLength == 0 ? 'text-slate-500' : 'text-guideOrange'} `}
-						>
-							{cartLength != 0 && (
-								<div
-									className='absolute top-[50%] right-[30%] md:right-[40%] w-4 h-4 rounded-full 
-															  text-xs bg-guideRed text-white animate-pulse'
-								>
-									{cartLength}
-								</div>
-							)}
-							<i>
-								<FaShoppingBag />
-							</i>
-						</button>
-					</Link>
-					<button
-						className={`relative flex flex-col items-center justify-center text-2xl h-full w-full 
-												 ${likesCounter == 0 ? 'text-slate-500' : 'text-guideOrange'} `}
-					>
-						{likesCounter != 0 && (
-							<div
-								className='absolute top-[50%] right-[30%] md:right-[40%] w-4 h-4 rounded-full 
-															  text-xs bg-guideRed text-white animate-pulse'
-							>
-								{likesCounter}
-							</div>
-						)}
-						<i>
-							<BsFillHeartFill />
-						</i>
-					</button>
-					<button className='flex items-center justify-center text-2xl h-full w-full text-slate-500'>
-						<i>
-							<BsFillBellFill />
-						</i>
-					</button>
+					<NavigationButtons />
 				</nav>
 
 				<UserAvatar />
--- src/components/Header/mobileHeader.jsx
+++ src/components/Header/mobileHeader.jsx
@@ -1,43 +1,19 @@
-import { collection, onSnapshot } from 'firebase/firestore'
-import { useSession } from 'next-auth/react'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { AiFillCompass } from 'react-icons/ai'
-import { BsFillBellFill, BsFillHeartFill } from 'react-icons/bs'
-import { FaShoppingBag } from 'react-icons/fa'
-import { MdPlace } from 'react-icons/md'
-import { useSelector } from 'react-redux'
-import { db } from '../../../firebase'
+import { useState } from 'react'
 import { SideProfile } from '../SideProfile'
 import { LogoPapasPizzeria } from '../UI/LogoPapasPizzeria'
+import { NavigationButtons } from './CommonParts/NavigationButtons'
 import { SideMenuButton } from '../UI/SideMenuButton'
 import { UserAvatar } from '../UI/UserAvatar'
 
 const MobileHeader = () => {
 	const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
-	const { data: session, status } = useSession()
-	const [likesCounter, setLikesCounter] = useState(0)
-	const cartLength = useSelector(state => state.cart.length)
-
-	useEffect(() => {
-		let unmounted = false
-		session &&
-			!unmounted &&
-			onSnapshot(
-				collection(db, 'users', session.user.firebaseID, 'likes'),
-				snapshot => setLikesCounter(snapshot.docs.length)
-			)
-		//Prevent a memory leak
-		return () => (unmounted = true)
-	}, [session])
 
 	return (
 		<>
 			{isSideMenuOpen && <SideProfile />}
 			<header
 				className='sticky top-0 w-full h-24 flex items-center justify-around
-			border-b-2 border-b-slate-300 bg-white lg:hidden
-			'
+			border-b-2 border-b-slate-300 bg-white lg:hidden'
 			>
 				<SideMenuButton
 					isSideMenuOpen={isSideMenuOpen}
@@ -48,63 +24,15 @@ const MobileHeader = () => {
 
 				<UserAvatar />
 			</header>
+
 			<footer
 				className='fixed bottom-0 w-full flex items-center 
 			h-16 justify-around border-t-2 border-t-slate-300 bg-white lg:hidden'
 			>
-				<button className='flex items-center justify-center text-2xl h-full w-full text-slate-500'>
-					<i>
-						<AiFillCompass />
-					</i>
-				</button>
-				<button className='flex items-center justify-center text-2xl h-full w-full text-slate-500'>
-					<i>
-						<MdPlace />
-					</i>
-				</button>
-				<Link href='/cart' passHref>
-					<button
-						className={`relative flex flex-col items-center justify-center text-2xl h-full w-full 
-											 ${cartLength == 0 ? 'text-slate-500' : 'text-guideOrange'} `}
-					>
-						{cartLength != 0 && (
-							<div
-								className='absolute top-[50%] right-[30%] md:right-[40%] w-4 h-4 
-															rounded-full text-xs bg-guideRed text-white animate-pulse'
-							>
-								{cartLength}
-							</div>
-						)}
-						<i>
-							<FaShoppingBag />
-						</i>
-					</button>
-				</Link>
-				<button
-					className={`relative flex flex-col items-center justify-center text-2xl h-full w-full 
-											 ${likesCounter == 0 ? 'text-slate-500' : 'text-guideOrange'} `}
-				>
-					{likesCounter != 0 && (
-						<div
-							className='absolute top-[50%] right-[30%] md:right-[40%] w-4 h-4 
-															rounded-full text-xs bg-guideRed text-white animate-pulse'
-						>
-							{likesCounter}
-						</div>
-					)}
-					<i>
-						<BsFillHeartFill />
-					</i>
-				</button>
-				<button className='flex items-center justify-center text-2xl h-full w-full text-slate-500'>
-					<i>
-						<BsFillBellFill />
-					</i>
-				</button>
+				<NavigationButtons />
 			</footer>
 		</>
 	)
 }
 
 export { MobileHeader }
-
