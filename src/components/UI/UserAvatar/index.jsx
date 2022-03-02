5cf6c650a3eff1fdfe629feb50b8282683297637
MSpilari <mb.spilari@hotmail.com>
2022-03-02 11:52:16 -0300

    Criando o componente comum para o avatar do usuário

---
+++ src/components/UI/UserAvatar/index.jsx
@@ -0,0 +1,32 @@
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+import { BsPersonFill } from 'react-icons/bs'
+
+const UserAvatar = ({ sideProfile }) => {
+	const { data: session, status } = useSession()
+
+	return status == 'authenticated' ? (
+		<div
+			className={`relative mr-3 h-14 w-16 rounded-full overflow-hidden lg:mx-5
+                 ${sideProfile && 'mt-3 h-20 w-20 mr-0 lg:mx-0'}`}
+		>
+			<Image
+				sizes='50vw'
+				src={session.user.image}
+				layout='fill'
+				alt='User Pic'
+			/>
+		</div>
+	) : (
+		<div
+			className={`relative mx-5 h-14 w-16 flex items-center justify-center
+                    ${sideProfile && 'mt-3 h-20 w-20 mr-0 lg:mx-0'}`}
+		>
+			<i className='text-4xl text-slate-500'>
+				<BsPersonFill />
+			</i>
+		</div>
+	)
+}
+
+export { UserAvatar }
