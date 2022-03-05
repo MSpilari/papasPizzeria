65587681e776662d384803aadaaa247123df1262
MSpilari <mb.spilari@hotmail.com>
2022-03-05 11:25:10 -0300

    Finalizando o Css do formulário

--- src/components/AdminForm/index.jsx
+++ src/components/AdminForm/index.jsx
@@ -15,16 +15,34 @@ const AdminForm = ({ setIsAdminFormOpen }) => {
 					<AiOutlineClose />
 				</button>
 				<h1 className='text-xl text-white mx-1'>Hi admin, sign in, please.</h1>
-				<form action=''>
-					<span>
-						<label htmlFor='email'>Email:</label>
-						<input type='email' name='email' />
+				<form action='' className='flex flex-col w-full'>
+					<span className='w-[90%] flex items-center text-white mx-auto my-2'>
+						<label className='w-28' htmlFor='email'>
+							Email:
+						</label>
+						<input
+							className='w-full text-guideOrange outline-none rounded-md bg-transparent border-2 border-guideOrangeLight'
+							type='email'
+							name='email'
+						/>
 					</span>
-					<span>
-						<label htmlFor='password'>Password:</label>
-						<input type='password' name='password' />
+					<span className='w-[90%] flex items-center text-white mx-auto my-2'>
+						<label className='w-28' htmlFor='password'>
+							Password:
+						</label>
+						<input
+							className='w-full text-guideOrange outline-none rounded-md bg-transparent border-2 border-guideOrangeLight'
+							type='password'
+							name='password'
+						/>
 					</span>
-					<button type='submit'>Send</button>
+					<button
+						type='submit'
+						className='my-3 ml-auto mr-4 
+						bg-white rounded-3xl px-5 py-2 text-sm text-guideOrange font-bold'
+					>
+						Sign in as Admin
+					</button>
 				</form>
 			</section>
 		</div>
