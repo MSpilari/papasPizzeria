b9ee98869b4dd34d7e7bc0d8c722c29164c1577f
MSpilari <mb.spilari@hotmail.com>
2022-03-08 12:57:04 -0300

    Inserindo o slice do admin no store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../slices/CartSlice'
+import adminReducer from '../slices/AdminSlice'
 
 export default configureStore({
 	reducer: {
-		cart: cartReducer
+		cart: cartReducer,
+		admin: adminReducer
 	}
 })
