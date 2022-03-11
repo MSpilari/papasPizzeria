c24f75dc0a004ced7bc65ad1ae8457ec2f97a8da
MSpilari <mb.spilari@hotmail.com>
2022-03-11 17:07:10 -0300

    Adicionando o Credentials provider e as condicionais nas callbacks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
@@ -6,7 +7,31 @@ export default NextAuth({
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID,
-			clientSecret: process.env.GOOGLE_SECRET
+			clientSecret: process.env.GOOGLE_SECRET,
+			id: 'google'
+		}),
+		CredentialsProvider({
+			name: 'Credentials',
+			id: 'credentials',
+			credentials: {
+				email: { label: 'Email', type: 'text' },
+				password: { label: 'Password', type: 'password' }
+			},
+			authorize: async (credentials, req) => {
+				const { email, password } = credentials
+
+				const res = await fetch(`${process.env.NEXTAUTH_URL}/api/admin/login`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ email, password })
+				})
+
+				const admin = await res.json()
+
+				if (res.ok && admin) return admin
+
+				return null
+			}
 		})
 		// ...add more providers here
 	],
@@ -15,31 +40,38 @@ export default NextAuth({
 	},
 	callbacks: {
 		async session({ session, token, user }) {
-			const res = await fetch(
-				`${process.env.NEXTAUTH_URL}/api/users/findUserId`,
-				{
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({ email: session.user.email })
-				}
-			)
-
-			const data = await res.json()
-			session.user.firebaseID = data.userID
+			if (session.user.name != undefined) {
+				const res = await fetch(
+					`${process.env.NEXTAUTH_URL}/api/users/findUserId`,
+					{
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({ email: session.user.email })
+					}
+				)
+				const data = await res.json()
+				session.user.firebaseID = data.userID
+			}
 			return session
 		},
 		async signIn({ account, email, profile, user, credentials }) {
-			const res = await fetch(`${process.env.NEXTAUTH_URL}/api/users/addUser`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(user)
-			})
-			const data = await res.json()
-			return data.message
+			if (account.provider != 'credentials') {
+				const res = await fetch(
+					`${process.env.NEXTAUTH_URL}/api/users/addUser`,
+					{
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(user)
+					}
+				)
+				const data = await res.json()
+				return data.message
+			}
+			return true
 		}
 	},
 	secret: process.env.NEXTAUTH_SECRET
