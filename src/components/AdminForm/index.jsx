66fb0e52295449ca900f4a9d05a245072b6d1b59
MSpilari <mb.spilari@hotmail.com>
2022-03-11 17:10:17 -0300

    Alterando a forma de login do firebase/auth para o next-auth

--- src/components/AdminForm/index.jsx
+++ src/components/AdminForm/index.jsx
@@ -1,10 +1,6 @@
-import { useEffect, useRef, useState } from 'react'
-import { useRouter } from 'next/router'
+import { useRef, useState } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../../../firebase'
-import { useDispatch, useSelector } from 'react-redux'
-import { adminLoggedIn } from '../../redux/slices/AdminSlice'
+import { signIn } from 'next-auth/react'
 
 const AdminForm = ({ setIsAdminFormOpen }) => {
 	const [adminCredentials, setAdminCredentials] = useState({})
@@ -12,27 +8,7 @@ const AdminForm = ({ setIsAdminFormOpen }) => {
 	const emailInputRef = useRef()
 	const passwordInputRef = useRef()
 
-	const router = useRouter()
-
-	const dispatch = useDispatch()
-	const adminLength = useSelector(state => state.admin.length)
-
-	const adminLogin = async ({ email, password }) => {
-		try {
-			const isSignedIn = await signInWithEmailAndPassword(auth, email, password)
-			const { accessToken, email: adminEmail, uid } = isSignedIn.user
-
-			dispatch(adminLoggedIn({ accessToken, adminEmail, uid }))
-		} catch (error) {
-			setErrorMessage(true)
-			emailInputRef.current.value = ''
-			passwordInputRef.current.value = ''
-		}
-	}
-
-	useEffect(() => {
-		if (adminLength > 0) router.push('/admin/configs')
-	}, [adminLength, router])
+	const adminLogin = async () => {}
 
 	return (
 		<div className='absolute top-0 w-full h-full z-10 bg-[rgba(0,0,0,0.9)] flex items-center justify-center'>
@@ -54,14 +30,7 @@ const AdminForm = ({ setIsAdminFormOpen }) => {
 						The email/password are not correct !
 					</p>
 				)}
-				<form
-					action=''
-					className='flex flex-col w-full'
-					onSubmit={e => {
-						e.preventDefault()
-						return adminLogin(adminCredentials)
-					}}
-				>
+				<form className='flex flex-col w-full'>
 					<span className='w-[90%] flex items-center text-white mx-auto my-2'>
 						<label className='w-28' htmlFor='email'>
 							Email:
@@ -101,9 +70,15 @@ const AdminForm = ({ setIsAdminFormOpen }) => {
 					</span>
 
 					<button
-						type='submit'
+						type='button'
 						className='my-3 ml-auto mr-4 
 						bg-white rounded-3xl px-5 py-2 text-sm text-guideOrange font-bold'
+						onClick={() =>
+							signIn('credentials', {
+								callbackUrl: '/admin/configs',
+								...adminCredentials
+							})
+						}
 					>
 						Sign in as Admin
 					</button>
