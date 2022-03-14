2746cbb0d5e101ffdb7eccbefa948c459471150e
MSpilari <mb.spilari@hotmail.com>
2022-03-14 16:55:36 -0300

    Inserindo a renderização condicional caso o admin não esteja logado

--- pages/admin/configs.js
+++ pages/admin/configs.js
@@ -1,7 +1,29 @@
 import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { PizzasTaxes } from '../../src/components/PizzasTaxes'
 
 const Configs = () => {
-	return <h1>Admin Configs</h1>
+	const { data: session, status } = useSession()
+	return (
+		<main className='w-screen h-screen'>
+			<div className='w-full h-full'>
+				<div className='w-full'>
+					{status == 'authenticated' && session.user.name == undefined ? (
+						<PizzasTaxes />
+					) : (
+						<div className='w-full h-full flex flex-col items-center justify-center'>
+							<h1>Sorry, you are not a admin</h1>
+							<Link href='/login' passHref>
+								<button className='bg-white rounded-3xl px-5 py-2 text-sm text-guideOrange border-2 border-guideOrange font-bold'>
+									To Login Page
+								</button>
+							</Link>
+						</div>
+					)}
+				</div>
+			</div>
+		</main>
+	)
 }
 
 export default Configs
