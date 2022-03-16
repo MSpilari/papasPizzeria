e655731b0ed6230674f861a0472eca952bd7f443
MSpilari <mb.spilari@hotmail.com>
2022-03-16 17:12:02 -0300

    Criando os campos do formulário como name, image, description, price e optionals

--- src/components/PizzasTaxes/components/AddNewPizzaForm/index.jsx
+++ src/components/PizzasTaxes/components/AddNewPizzaForm/index.jsx
@@ -1,9 +1,79 @@
+import { useState } from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+
 const AddNewPizzaForm = () => {
+	const optionalField = (
+		<li key={nanoid()}>
+			<label htmlFor='ingredient'>Ingredient:</label>
+			<input
+				className='border-2 border-slate-200 outline-none rounded-lg'
+				type='text'
+				name='ingredient'
+			/>
+			<label htmlFor='optPrice'>optPrice:</label>
+			<input
+				className='border-2 border-slate-200 outline-none rounded-lg'
+				type='text'
+				name='optPrice'
+			/>
+		</li>
+	)
+
+	const initialOptionField = [optionalField]
+
+	const [optionFields, setOptionFields] = useState(initialOptionField)
 	return (
-		<section>
+		<section className='w-[90%] mx-auto border-2 border-slate-200 rounded-lg'>
 			<h1 className='text-center'>Add a new Pizza</h1>
-			<form>
-				{/*Formulário que receberá os dados de cadastro de uma nova pizza */}
+			<form className='flex flex-col w-[90%] mx-auto'>
+				<input type='file' name='image' />
+				<div className='flex items-center my-1'>
+					<label htmlFor='name'>Name:</label>
+					<input
+						className='border-2 border-slate-200 outline-none rounded-lg'
+						type='text'
+						name='name'
+					/>
+				</div>
+				<div className='flex items-center my-1'>
+					<label htmlFor='description'>Description:</label>
+					<textarea
+						className='border-2 border-slate-200 outline-none rounded-lg resize-none'
+						name='description'
+					/>
+				</div>
+				<div className='flex items-center my-1'>
+					<label htmlFor='price'>Price:</label>
+					<input
+						className='border-2 border-slate-200 outline-none rounded-lg'
+						type='text'
+						name='price'
+					/>
+				</div>
+				<div className='my-1'>
+					<h4>Optional</h4>
+					<button
+						disabled={optionFields.length == 1}
+						type='button'
+						onClick={() =>
+							setOptionFields(prevState => {
+								const newArr = prevState.slice(0, prevState.length - 1)
+								return newArr
+							})
+						}
+					>
+						-
+					</button>
+					<button
+						type='button'
+						onClick={() =>
+							setOptionFields(prevState => [...prevState, optionalField])
+						}
+					>
+						+
+					</button>
+					<ul>{optionFields}</ul>
+				</div>
 			</form>
 		</section>
 	)
