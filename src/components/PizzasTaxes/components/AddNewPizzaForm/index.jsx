039aae1c45dfc7a67ad9439d40ea886cee796b62
MSpilari <mb.spilari@hotmail.com>
2022-03-16 11:36:19 -0300

    Componentizando o formulário de adição de uma nova pizza no firebase

---
+++ src/components/PizzasTaxes/components/AddNewPizzaForm/index.jsx
@@ -0,0 +1,12 @@
+const AddNewPizzaForm = () => {
+	return (
+		<section>
+			<h1 className='text-center'>Add a new Pizza</h1>
+			<form>
+				{/*Formulário que receberá os dados de cadastro de uma nova pizza */}
+			</form>
+		</section>
+	)
+}
+
+export { AddNewPizzaForm }
