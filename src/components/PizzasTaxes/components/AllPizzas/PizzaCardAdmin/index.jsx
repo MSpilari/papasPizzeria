c7273fe8bb36a29d5e63afb3570856e90e483a02
MSpilari <mb.spilari@hotmail.com>
2022-03-17 17:27:49 -0300

    Colocando a renderização condicional na imagem do pizzaAdminCard para evitar um erro por falta do src da imagem

--- src/components/PizzasTaxes/components/AllPizzas/PizzaCardAdmin/index.jsx
+++ src/components/PizzasTaxes/components/AllPizzas/PizzaCardAdmin/index.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { AiOutlineClose } from 'react-icons/ai'
+import { Loading } from '../../../../Loading'
 
 const PizzaCardAdmin = ({ itemInfo }) => {
 	const { image, name } = itemInfo
@@ -8,13 +9,17 @@ const PizzaCardAdmin = ({ itemInfo }) => {
 		<section className='w-[90%] mx-auto mt-3 border-2 border-slate-200 rounded-lg'>
 			<div className='w-full flex items-center'>
 				<div className='relative w-24 h-24 rounded-2xl overflow-hidden mx-2 my-1'>
-					<Image
-						layout='fill'
-						objectFit='fill'
-						sizes='50vw'
-						src={image}
-						alt={`${name} Pic`}
-					/>
+					{image ? (
+						<Image
+							layout='fill'
+							objectFit='fill'
+							sizes='50vw'
+							src={image}
+							alt={`${name} Pic`}
+						/>
+					) : (
+						<Loading />
+					)}
 				</div>
 				<div className='w-[calc(100%-150px)] flex flex-col'>
 					<h3 className='w-full truncate text-lg'>{name}</h3>
