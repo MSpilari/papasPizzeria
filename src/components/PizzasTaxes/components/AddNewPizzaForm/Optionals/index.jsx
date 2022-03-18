e0c38ae6563e296c20305c1d2ed1a37506a20e1f
MSpilari <mb.spilari@hotmail.com>
2022-03-18 17:52:09 -0300

    Separando os optionals do arquivo principal para facilitar o entendimento do código

---
+++ src/components/PizzasTaxes/components/AddNewPizzaForm/Optionals/index.jsx
@@ -0,0 +1,67 @@
+import { useRef } from 'react'
+import { handleInputChanges } from '../../../../../utils/handleInputChanges'
+
+const Optionals = ({
+	optionals,
+	setOptionals,
+	newOptionals,
+	setNewOptionals
+}) => {
+	const ingredientRef = useRef(null)
+	const optPriceRef = useRef(null)
+
+	return (
+		<div className='my-1'>
+			<h4>Optionals</h4>
+
+			<ul className='w-[95%] mx-auto'>
+				{optionals.map((opt, index) => (
+					<li key={index} className='flex my-1'>
+						<p>Ingredient: {opt.ingredient}</p>
+						<p className='ml-2'>optPrice: {opt.optPrice}</p>
+					</li>
+				))}
+			</ul>
+
+			<label htmlFor='ingredient'>Ingredient:</label>
+			<input
+				ref={ingredientRef}
+				className='border-2 border-slate-200 outline-none rounded-lg'
+				type='text'
+				name='ingredient'
+				onChange={e => handleInputChanges(e, setNewOptionals)}
+			/>
+
+			<label htmlFor='optPrice'>optPrice:</label>
+			<input
+				ref={optPriceRef}
+				className='border-2 border-slate-200 outline-none rounded-lg'
+				type='number'
+				name='optPrice'
+				onChange={e => handleInputChanges(e, setNewOptionals)}
+			/>
+
+			<button
+				className='px-2 border-2 border-slate-300 rounded-lg ml-1 disabled:text-gray-300 disabled:cursor-not-allowed'
+				disabled={
+					optPriceRef.current &&
+					ingredientRef.current &&
+					(optPriceRef.current.value == '' || ingredientRef.current.value == '')
+				}
+				type='button'
+				onClick={() => {
+					setOptionals(prevState => [...prevState, newOptionals])
+					if (optPriceRef.current && ingredientRef.current) {
+						optPriceRef.current.value = ''
+						ingredientRef.current.value = ''
+					}
+					setNewOptionals({})
+				}}
+			>
+				Ok
+			</button>
+		</div>
+	)
+}
+
+export { Optionals }
