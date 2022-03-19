a56d807ec20c5daed37e54758172c2580a700d79
MSpilari <mb.spilari@hotmail.com>
2022-03-19 13:58:31 -0300

    Inserindo o botão de signOut para o admin

--- src/components/PizzasTaxes/components/AllPizzas/index.jsx
+++ src/components/PizzasTaxes/components/AllPizzas/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { onSnapshot, collection } from 'firebase/firestore'
+import { signOut } from 'next-auth/react'
 import { db } from '../../../../../firebase'
 import { PizzaCardAdmin } from './PizzaCardAdmin'
 
@@ -20,8 +21,14 @@ const AllPizzas = () => {
 	}, [])
 
 	return (
-		<section className='w-full h-full my-1'>
+		<section className='w-full h-full my-1 flex flex-col items-center'>
 			<h1 className='text-center'>All Pizzas</h1>
+			<button
+				onClick={() => signOut({ callbackUrl: '/login' })}
+				className='border-2 border-slate-500 my-2 rounded-lg py-1 px-2'
+			>
+				Sign Out
+			</button>
 			{allPizzas.map(pizza => (
 				<PizzaCardAdmin key={pizza.firebaseId} itemInfo={pizza} />
 			))}
