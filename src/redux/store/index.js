335b804624d4c9a6ebaf56951450e99606d10f4d
MSpilari <mb.spilari@hotmail.com>
2022-03-19 12:15:25 -0300

    Retirando o slice admin, pois ele não estava sendo usado

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../slices/CartSlice'
-import adminReducer from '../slices/AdminSlice'
 
 export default configureStore({
 	reducer: {
-		cart: cartReducer,
-		admin: adminReducer
+		cart: cartReducer
 	}
 })
