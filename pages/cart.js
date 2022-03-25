76405858ac8ec18046add5d7864c0cc776abafb2
MSpilari <mb.spilari@hotmail.com>
2022-03-25 16:58:55 -0300

    Fazendo a chamada para a API e repassando os dados para a página do Cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,17 +1,24 @@
 import { CartWrapper } from '../src/components/Cart'
 import { Header } from '../src/components/Header'
 
-const Cart = () => {
+const Cart = ({ data }) => {
 	return (
 		<main className='w-screen h-screen'>
 			<div className='w-full h-full'>
 				<Header />
 				<div className='w-full h-[calc(100%-160px)] lg:h-[calc(100%-96px)] overflow-y-auto'>
-					<CartWrapper />
+					<CartWrapper taxes={data} />
 				</div>
 			</div>
 		</main>
 	)
+}
+
+export async function getServerSideProps() {
+	const res = await fetch(`${process.env.NEXTAUTH_URL}/api/taxes`)
+	const data = await res.json()
+
+	return { props: { data } }
 }
 
 export default Cart
